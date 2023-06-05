a3a2e6642da8c1405058c1f444f86132a8c429ef
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-06-05 23:03:03 +0800

    Mon Jun  5 11:03:03 PM CST 2023

---
+++ script.js
@@ -0,0 +1,28 @@
+
+var currentURL = window.location.href;
+
+// Create a URLSearchParams object
+var params = new URLSearchParams(new URL(currentURL).search);
+
+// Get the value of the "cell" parameter
+var cellValue = params.get("cell");
+
+// Print the value
+if (cellValue) {
+  let apiURL = 'https://script.google.com/macros/s/AKfycbyemCrwxJ8FqhJzcTwf6OWCqBofEvXHeHB3X9sC7liAtnxrS1H2P2ktOIkaFp0CipKXMQ/exec'
+  fetch(`$apiURL?cell=${cellValue}`)
+    .then(response => response.text())
+    .then(data => {
+      // console.log(data); // Process the raw text data
+      if (data.startsWith('http')) {
+        location.href = data
+      }
+      else {
+        console.error(data)
+      }  
+    })
+    .catch(error => {
+      console.log('Error:', error);
+    });
+
+}
