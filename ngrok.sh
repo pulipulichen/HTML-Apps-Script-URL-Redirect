f5fd95bfd49a117ef97d9606c891f6a98c8e042d
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-06-05 23:48:26 +0800

    Mon Jun  5 11:48:26 PM CST 2023

--- ngrok.sh
+++ ngrok.sh
@@ -3,4 +3,4 @@ ngrok http $PORT --log=stdout > /tmp/ngrok.log &
 sleep 3
 file_content=$(cat /tmp/ngrok.log)
 url=$(echo "$file_content" | grep -o 'http[s]\?://[^[:space:]]\+' | grep -v '^http://localhost')
-curl -X POST "$API" -d "url=$url&cell=$CELL>
+curl -X POST "$API" -d "url=$url&cell=$CELL"
