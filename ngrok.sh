c6dd05f6eb08c5406c349f57c6e5e390e9cb9833
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-06-06 10:32:23 +0800

    Tue Jun  6 10:32:23 AM CST 2023

--- ngrok.sh
+++ ngrok.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ngrok http $PORT --log=stdout > /tmp/ngrok.log &
+/usr/local/bin/ngrok http $PORT --log=stdout > /tmp/ngrok.log &
 sleep 3
 file_content=$(cat /tmp/ngrok.log)
 url=$(echo "$file_content" | grep -o 'http[s]\?://[^[:space:]]\+' | grep -v '^http://localhost')
